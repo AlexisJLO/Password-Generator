f1c3b77bc93a9a48b263ded4a1fb330d6d75e0a9
Alexis Lopez <lopez111alex@gmail.com>
2023-09-05 10:10:39 -0700

    Added confirm prompts asking if they want to include uppercase, lowercase, numerical and special characters

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,8 @@ function generatePassword() {
     var uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     var numericChars = '0123456789';
     var specialChars = '!@#$%^&*()_+~`|}{[]:;?><,./-=';
+    var charSet = '';
+    var password = '';
   
    
     var passwordLength = prompt("Enter the length of the password (between 8 and 128 characters):");
@@ -12,6 +14,12 @@ function generatePassword() {
     if (passwordLength < 8 || passwordLength > 128) {
       alert("Password length must be between 8 and 128 characters.");
     }
+  
+    var includeLowercase = confirm("Include lowercase characters?");
+    var includeUppercase = confirm("Include uppercase characters?");
+    var includeNumeric = confirm("Include numeric characters?");
+    var includeSpecial = confirm("Include special characters?");
+
   
   }
   
