852d39b0eb67c38778b31c97cf8e765143c220a0
Alexis Lopez <lopez111alex@gmail.com>
2023-09-05 10:01:49 -0700

    prompted the lenght on password

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,30 @@
-// Assignment code here
+function generatePassword() {
+   
+    var lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    var uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var numericChars = '0123456789';
+    var specialChars = '!@#$%^&*()_+~`|}{[]:;?><,./-=';
+  
+   
+    var passwordLength = prompt("Enter the length of the password (between 8 and 128 characters):");
+  
+    
+    if (passwordLength < 8 || passwordLength > 128) {
+      alert("Password length must be between 8 and 128 characters.");
+    }
+  
+  }
+  
 
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  var generateBtn = document.querySelector("#generate");
+  
+ 
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  }
+  
+  generateBtn.addEventListener("click", writePassword);
+  
